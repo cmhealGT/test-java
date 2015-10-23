23b544993ec8f0a15e426ad21b3f66e1a7feccb4
chealey <chris.healey@schneider-electric.com>
2015-10-23 13:03:31 -0400

    test-java-3 Change class to allow for any upper limit to be tested

--- maven/src/main/java/runPrimeSieve.java
+++ maven/src/main/java/runPrimeSieve.java
@@ -4,9 +4,13 @@
  */
 public class runPrimeSieve {
     public static void main(String[] args) {
-        int number = Integer.parseInt(args[0]);
+        //int number = Integer.parseInt(args[0]);
+        int number = 100;
         PrimeSieve sieve = new PrimeSieve(number);
-        int result = sieve.returnNumOfPrimes();
-        System.out.println("The number of primes <= " + number + " is " + result);
+        int result = sieve.returnNumOfPrimes(number);
+        System.out.println("The number of primes less than " + number + " is " + result);
+        number = 800;
+        result = sieve.returnNumOfPrimes(800);
+        System.out.println("The number of primes less than " + number + " is " + result);
     }
 }
